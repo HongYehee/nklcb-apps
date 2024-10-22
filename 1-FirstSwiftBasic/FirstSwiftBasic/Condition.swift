5e915b6956a842e34cffcdf86d99f0ecd25a3ec6
yh.hong <ghddpgml@gmail.com>
2024-10-22 17:48:49 +0900

    Part 1. Ch 2. 조건문

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/Condition.swift
@@ -0,0 +1,18 @@
+//
+//  Condition.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/22/24.
+//
+
+import SwiftUI
+
+struct Condition: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Condition()
+}
