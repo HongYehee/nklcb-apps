15b7e05780a8233b6492d5481833f240dba3fad0
yh.hong <ghddpgml@gmail.com>
2024-10-22 17:31:29 +0900

    Part 1. Ch 2. 컬렉션

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/MyCollections.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollections.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/22/24.
+//
+
+import SwiftUI
+
+struct MyCollections: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCollections()
+}
