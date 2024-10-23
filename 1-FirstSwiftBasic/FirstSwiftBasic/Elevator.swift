6de453b04650eef23a87f1b4c5784055f9640684
yh.hong <ghddpgml@gmail.com>
2024-10-23 10:39:47 +0900

    Part 1. Ch 3. 구조체

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/Elevator.swift
@@ -0,0 +1,18 @@
+//
+//  Elevator.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/23/24.
+//
+
+import SwiftUI
+
+struct Elevator: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Elevator()
+}
