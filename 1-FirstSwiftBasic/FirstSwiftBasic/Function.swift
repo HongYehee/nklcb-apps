af5f872ff1e1e5c11b3422dd8c79de610ebba727
yh.hong <ghddpgml@gmail.com>
2024-10-23 10:03:46 +0900

    Part 1. Ch 2. 함수

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/Function.swift
@@ -0,0 +1,18 @@
+//
+//  Function.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/23/24.
+//
+
+import SwiftUI
+
+struct Function: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Function()
+}
