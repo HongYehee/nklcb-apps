a3c92524504e124e58bdc44e948c3695797f336b
yh.hong <ghddpgml@gmail.com>
2024-10-23 14:42:39 +0900

    Part 1. Ch 3. 스위치

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/Selection.swift
@@ -0,0 +1,18 @@
+//
+//  Selection.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/23/24.
+//
+
+import SwiftUI
+
+struct Selection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Selection()
+}
