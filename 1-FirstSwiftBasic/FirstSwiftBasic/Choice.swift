895acc28d6aeb00780205f8a4fed822057027e47
yh.hong <ghddpgml@gmail.com>
2024-10-23 14:19:57 +0900

    Part 1. Ch 3. 열거형

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/Choice.swift
@@ -0,0 +1,18 @@
+//
+//  Choice.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/23/24.
+//
+
+import SwiftUI
+
+struct Choice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Choice()
+}
