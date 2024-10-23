a9ed07e64709498ab45a2a95c61ab7d101d6cb7d
yh.hong <ghddpgml@gmail.com>
2024-10-23 09:47:40 +0900

    Part 1. Ch 2. 반복문

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/Loop.swift
@@ -0,0 +1,18 @@
+//
+//  Loop.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/23/24.
+//
+
+import SwiftUI
+
+struct Loop: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loop()
+}
