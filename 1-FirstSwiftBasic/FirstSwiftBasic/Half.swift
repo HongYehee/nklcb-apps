8e92feadad6dbc92b3d6064dc60e6c848618299b
yh.hong <ghddpgml@gmail.com>
2024-10-23 10:23:52 +0900

    Part 1. Ch 2. 중간 점검해보기

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/Half.swift
@@ -0,0 +1,18 @@
+//
+//  Half.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/23/24.
+//
+
+import SwiftUI
+
+struct Half: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Half()
+}
