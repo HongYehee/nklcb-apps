540e8f0a15bf6edf50a603fd30ef931505a4f594
yh.hong <ghddpgml@gmail.com>
2024-10-24 15:19:22 +0900

    Part 1. Ch 3. 옵셔널 바인딩, 옵셔널 체이닝

---
+++ 1-FirstSwiftBasic/FirstSwiftBasic/Nil.swift
@@ -0,0 +1,18 @@
+//
+//  Nil.swift
+//  FirstSwiftBasic
+//
+//  Created by 홍예희 on 10/24/24.
+//
+
+import SwiftUI
+
+struct Nil: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Nil()
+}
